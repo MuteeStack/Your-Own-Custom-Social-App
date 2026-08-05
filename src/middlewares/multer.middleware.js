0d7eb32f34663bba8d51b28e21e496165292d19b
MuteeStack <muteestacks@gmail.com>
2026-08-05 08:50:50 +0500

    multer config complete

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,1 +1,13 @@
 import multer from "multer"
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './Public/Temp')
+  },
+  filename: function (req, file, cb) { // write filename by which to save in temp folder
+    cb(null, file.filename)
+  }
+})
+
+export const upload = multer({ storage, })
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (fun) => async (req , res , next) => {
    try {
         await fun(req , res , next)
    } catch (error) {
-    res.status(error.code || 500).json({
+        res.status(error.code || 500).json({
         sucess: false,
         message: error.message
     })
