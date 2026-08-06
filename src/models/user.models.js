fbee199cdbbb541f16f0564c1c71c2fabf1dc272
MuteeStack <muteestacks@gmail.com>
2026-08-06 08:40:51 +0500

    User Controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -57,7 +57,6 @@ userSchema.pre("save" , async function(next){
     if(!this.isModified("password")) return next();
         this.password = bcrypt.hash(this.password , 10)
         next()
-
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
@@ -71,7 +70,6 @@ userSchema.methods.generateAccessToken = function(){
         userName: this.userName,
         fullname: this.fullname,
         email : this.email
-
     } ,
     process.env.ACCESS_TOKEN_SECRET
     ,{
@@ -82,7 +80,6 @@ userSchema.methods.generateAccessToken = function(){
 userSchema.methods.generateRefreshToken = function(){
     jwt.sign({
         _id : this._id
-
     } ,
     process.env.REFRESH_TOKEN_SECRET
     ,{
