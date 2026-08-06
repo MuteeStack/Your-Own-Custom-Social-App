cf367c15f90aa059aa54ba21ddfffa6d4625ac7a
MuteeStack <muteestacks@gmail.com>
2026-08-06 17:08:04 +0500

    .

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,11 +1,14 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const regUser = asyncHandler(async (req , res)=> {
-    res.status(200).json(
-        {
-            message: "Mutee Ur Rehman is Good to Go"
-        }
-    )
+
+    // get user detail from frontend
+    // validation (like if user send empty username or passowrd like adding checks)
+    // Check that user already exsists
+    // check the images like the profile pic avator but the cover is option but avator is must so
+    // upload avator on cloudinary , also check if uploaded
+    // create user object // because we use object to send data to mongodb
+    
 })
 
 
