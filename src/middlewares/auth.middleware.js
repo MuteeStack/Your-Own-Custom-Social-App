1e7743da481974feeeb7cdbb29682031622aa180
MuteeStack <muteestacks@gmail.com>
2026-08-13 12:02:48 +0500

    .

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler";
 
 
-export const verfiyJWT = asyncHandler(async (req , res) => {
+export const verfiyJWT = asyncHandler(async (req , res , next) => {
 
 })
